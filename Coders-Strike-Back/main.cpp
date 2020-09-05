8ce776140131e826f8dd334cdaf976397d54fe19
caesiumCode <caesiumcode@aol.com>
2020-09-05 10:58:23 +0200

    Initial Commit

---
+++ Coders-Strike-Back/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coders-Strike-Back
+//
+//  Created by Stephen Jaud on 05/09/2020.
+//  Copyright © 2020 Stephen Jaud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
