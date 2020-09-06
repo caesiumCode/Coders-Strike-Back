ebc7fb54bbd2051e2bbdf51583187634b41f4e42
caesiumCode <caesiumcode@aol.com>
2020-09-06 23:22:16 +0200

    Setup Entity class

---
+++ Coders-Strike-Back/Entity.cpp
@@ -0,0 +1,9 @@
+#include "Entity.hpp"
+
+Entity::Entity() {
+    x = 0.;
+    y = 0.;
+    radius = 0.;
+    color = sf::Color::Black;
+}
+
---
+++ Coders-Strike-Back/Entity.hpp
@@ -0,0 +1,24 @@
+#ifndef Entity_hpp
+#define Entity_hpp
+
+#include <SFML/Graphics.hpp>
+
+class Entity : public sf::Drawable {
+public:
+    Entity();
+    
+private:
+    float x, y, radius;
+    sf::Color color;
+    
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+        sf::CircleShape texture;
+        texture.setRadius(radius);
+        texture.setPosition(x - radius, y - radius);
+        texture.setFillColor(color);
+        
+        target.draw(texture);
+    }
+};
+
+#endif /* Entity_hpp */
